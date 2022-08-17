ac6fb7d14dabcb4511d66cde7a167c894ba0794e
jb0403 <jayeshb7548@gmail.com>
2022-08-17 17:23:45 +0530

    React Interview Task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import HomePage from "./container/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomePage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
